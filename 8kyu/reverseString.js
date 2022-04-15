a5fdca56828f88187066da0f303a3d6c80dfbc46
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-04-15 23:11:32 +0200

    reverse string

---
+++ 8kyu/reverseString.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+function solution(str) {
+  return (str === '') ? '' : solution(str.substr(1)) + str.charAt(0)
+}
