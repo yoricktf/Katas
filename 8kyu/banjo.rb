6637d7e2e1ad6e52fbd2cddd5487677f40b94cd5
yoricktf <yorick.tenfeld@gmail.com>
2022-05-04 20:02:23 +0200

    simple case checking method

---
+++ 8kyu/banjo.rb
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/53af2b8861023f1d88000832/train/ruby
+
+def are_you_playing_banjo(name)
+  name[0].downcase == 'r' ? name + ' plays banjo' : name + ' does not play banjo'
+end
